49a3a8997c74129e3d666ddb96d23f21193e7b60
wmt36 <wmt36@Williams-iMac.local>
2020-08-25 09:17:26 -0600

    updated app.js file

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const newEmployee = []
 
 
 
+
 //figuring out positions
 const getPosition = [{
     type: 'list',
@@ -23,67 +24,106 @@ const getPosition = [{
     choices: [
         'Intern',
         'Manager',
-        'Engineer'
+        'Engineer',
+        'No more to add'
     ]
 }];
 
+
+//function to allow you to choose a role for employees
+function promptQuestions() {
+    inquirer.prompt(getPosition).then(answers => {
+        switch (answers.role) {
+            case "Manager":
+                return newManager();
+                break;
+
+            case "Intern":
+                return newIntern();
+                break;
+
+            case "Engineer":
+                return newEngineer();
+                break;
+                
+            case "No more to add":
+                return noMore();
+                break;
+        }
+
+
+
+
+    });
+
+};
+promptQuestions();
+
+
 //questions for the manager role
-const managerQuestion = inquirer.prompt([{
-        type: 'input',
-        name: 'name',
-        message: 'Team member name?'
-    },
-    {
-        type: 'input',
-        name: 'position',
-        message: 'What is positionon of the team member?'
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Enter email adress:'
-    },
-    {
-        type: 'input',
-        name: 'officeNumber',
-        message: 'Enter your Office Number:'
-    }
-
-
-]);
-
-// questions for the egnineer
-const engineerQuestion = inquirer.prompt([{
-        type: 'input',
-        name: 'name',
-        message: 'Team member name?'
-    },
-    {
-        type: 'input',
-        name: 'position',
-        message: 'What is the position of the team member?'
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Enter email adress:'
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: 'Enter GitHub link:'
-    }
-]);
-//questions for the intern 
-const internQuestion = inquirer.prompt([{
+const managerQuestion =
+    inquirer
+    .prompt([{
             type: 'input',
             name: 'name',
             message: 'Team member name?'
         },
         {
             type: 'input',
-            name: 'position',
-            message: 'What is the position of the team member?'
+            name: 'id',
+            message: 'id number:'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter email adress:'
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'Enter your Office Number:'
+        }
+
+
+    ]);
+
+// questions for the egnineer
+const engineerQuestion =
+    inquirer
+    .prompt([{
+            type: 'input',
+            name: 'name',
+            message: 'Team member name?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'id number:'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter email adress:'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter GitHub link:'
+        }
+    ]);
+
+//questions for the intern 
+const internQuestion =
+    inquirer
+    .prompt([{
+            type: 'input',
+            name: 'name',
+            message: 'Team member name?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'id number:'
         },
         {
             type: 'input',
@@ -95,12 +135,12 @@ const internQuestion = inquirer.prompt([{
             name: 'school',
             message: 'Enter School name:'
         }
-    
-
-]);
 
 
-//function to generate the html cards
+    ]);
+
+
+//function to generate the html cards with added input
 function newManager() {
     managerQuestion.then(answers => {
         const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber)
@@ -110,38 +150,44 @@ function newManager() {
 
 
 };
-//function to allow you to choose a role for employees
-function promptQuestions() {
-    inquirer.prompt(getPosition).then(answers => {
-        switch (answers.role) {
-            case "Manager":
-                newManager();
-                break;
 
-            case "Intern":
-                newIntern()
-                break;
-
-            case "Engineer":
-                engineerQuestion()
-                break;
-        }
+function newEngineer() {
+    engineerQuestion.then(answers => {
+        const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github)
+        newEmployee.push(engineer)
+    })
 
 
-
-
-    });
 
 };
 
-promptQuestions();
+function newIntern() {
+    internQuestion.then(answers => {
+        const intern = new Intern(answers.name, answers.id, answers.email, answers.school)
+        newEmployee.push(intern)
+    })
+
+
+
+};
+
+function noMore() {
+    renderHTML;
+}
+
+
+
+
+
+
+
 
 //function to generate the input needed to mark up the html cards
 function renderHTML() {
-    fs.writeFile(outputPath, render(newEmployee)), (err) => {
-        if (err) console.log(err)
-        else(console.log("successfully wrote to the team.html!"))
-    }
-}
+    fs.writeFile(outputPath, render(newEmployee))
 
-renderHTML();
+} //, (err) => {
+//         if (err) console.log(err)
+//         else(console.log("successfully wrote to the team.html!"))
+//     }
+// }
